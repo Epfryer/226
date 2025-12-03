7869f010409523617f982ddaf93c0e88891e8392
Epfryer <134826110+Epfryer@users.noreply.github.com>
2025-12-03 03:58:17 +0000

    studio shop

--- client/src/pages/Studio.tsx
+++ client/src/pages/Studio.tsx
@@ -464,7 +464,7 @@ export default function Studio() {
   }, []);
 
   return (
-    <div className="min-h-screen pt-18 pb-16">
+    <div className="min-h-screen pt-20 pb-16">
       <header className="fixed top-0 left-0 z-50 p-5">
         <motion.div
           initial={{ opacity: 0 }}
